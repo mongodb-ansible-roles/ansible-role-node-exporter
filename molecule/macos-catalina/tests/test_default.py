3a2e19a8e7d548381413bd3db2295e18e30fa2d2
John Chen <kryptonite303@users.noreply.github.com>
2020-05-11 10:33:56 -0400

    feat: Add rhel81-power8 support (#2)
    * feat: Add rhel81-power8 support
    
    Combined the static host tests into a single scenario, which tests to
    make sure it downloads the node_exporter and checks to make sure the
    service runs on the host by curling the /metrics endpoint.
    
    To test locally, make sure to add any hosts you want into your
    ~/.ssh/config.
    
    This systemd set up should work on any system that works with systemd.
    It runs node_exporter as the root user and saves the service file into
    /etc/systemd/system and the config file is saved to
    /etc/sysconfig/node_exporter.
    
    The tests will run on port 9101, so that it does not conflict with any
    existing instance of node_exporter running on the default port 9100.
    Also it saves the systemd file as node_exporter_test so that it does
    not conflict with any exising node_exporter systemd service.
    
    * fix: Replace macos-1014 with macos-catalina
    
    * fix: Add missing create step for macos-catalina
    
    * fix: Create.yml doesn't run on delegated instance
    
    * test: Add tests for macos-catalina
    
    * style: Adding new line
    
    * Debugging macos-catalina
    
    * build: Adding GH_TOKEN

--- molecule/macos-catalina/tests/test_default.py
+++ molecule/macos-catalina/tests/test_default.py
@@ -3,8 +3,8 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']
-).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
 def test_node_exporter(host):
@@ -12,3 +12,7 @@ def test_node_exporter(host):
     cmd = host.run("/tmp/node_exporter_test/node_exporter --version 2>&1 |"
                    "sed -n 1p | awk '{print $3}'")
     assert cmd.stdout == "1.0.0-rc.0\n"
+
+    cmd = host.run("curl localhost:9101/metrics | "
+                   "grep node_exporter_build_info")
+    assert cmd.succeeded
---
+++ molecule/static-hosts/tests/test_default.py
@@ -0,0 +1,18 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_node_exporter(host):
+    assert host.file("/tmp/node_exporter_test/node_exporter").exists
+    cmd = host.run("/tmp/node_exporter_test/node_exporter --version 2>&1 |"
+                   "sed -n 1p | awk '{print $3}'")
+    assert cmd.stdout == "1.0.0-rc.0\n"
+
+    cmd = host.run("curl localhost:9101/metrics | "
+                   "grep node_exporter_build_info")
+    assert cmd.succeeded
