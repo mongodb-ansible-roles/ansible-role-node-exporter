95fe400ce020211b7729e3fd7cd2bb88b27a0156
John Chen <kryptonite303@users.noreply.github.com>
2020-07-24 13:01:17 -0400

    feat: Adds support for upstart service manager (#7)
    * fix: Consolidated defaults into single file
    
    There are a set of reasonable defaults we can use across all service
    managers. The old service manager "specific" variables were hard coded
    since those will rarely, if ever change
    
    * feat: Add upstart support
    
    * test: Add service test for ephemeral hosts
    
    * fix: Replace old variables with more generic ones
    
    I was able to consolidate the various service specific variables into
    fewer generic variables and make the according changes to the ansible
    files.
    
    * docs: Replaced old variables with generic variables
    
    Also added notes on how static host testing works

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,6 @@ def test_node_exporter(host):
     cmd = host.run("/usr/local/bin/node_exporter --version 2>&1 |"
                    "sed -n 1p | awk '{print $3}'")
     assert cmd.stdout == "1.0.0-rc.0\n"
+    cmd = host.run("curl localhost:9100/metrics | "
+                   "grep node_exporter_build_info")
+    assert cmd.succeeded
